16ee00ea7d0e7c3bc485223776c398ff0f28ce5c
Joshua Melamed <jmelamed@adva.com>
2024-07-23 15:15:24 +0300

    Initial Commit

---
+++ src/main1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I am main1!\n";
+    return 0;
+}
---
+++ src/main2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I am main2!\n";
+    return 0;
+}
